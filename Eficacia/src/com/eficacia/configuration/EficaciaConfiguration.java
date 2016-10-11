e3ebf1a4d47de03de301bfca574b60137853b30c
ap77990 <ap77990@LACMX00EBAS0311.lac.nsroot.net>
2016-10-11 08:47:47 -0500

    borrar import sin usar en EficaciaConfiguration

--- Eficacia/src/com/eficacia/configuration/EficaciaConfiguration.java
+++ Eficacia/src/com/eficacia/configuration/EficaciaConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.context.support.ServletContextResource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
