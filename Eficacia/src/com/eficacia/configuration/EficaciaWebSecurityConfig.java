3ba26a5ee399687998b1353d5a3192b9f3cdd52a
ls77996 <ls77996@LACMX00EBAS0308.lac.nsroot.net>
2016-11-09 17:35:28 -0600

    Cambios permisos por usuario

--- Eficacia/src/com/eficacia/configuration/EficaciaWebSecurityConfig.java
+++ Eficacia/src/com/eficacia/configuration/EficaciaWebSecurityConfig.java
@@ -15,55 +15,51 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class EficaciaWebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class EficaciaWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	@Qualifier("customUserDetailsService")
 	UserDetailsService userDetailsService;
-	
+
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
 		auth.inMemoryAuthentication().withUser("user").password("12345").roles("ADMIN, EJECUTIVO");
-        
-        auth.userDetailsService(userDetailsService);
-        auth.authenticationProvider(authenticationProvider());
+
+		auth.userDetailsService(userDetailsService);
+		auth.authenticationProvider(authenticationProvider());
 	}
-	
+
 	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-     
-     
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
-	
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		return authenticationProvider;
+	}
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
+	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-        http.authorizeRequests().antMatchers("/", "/welcome", "/login", "/logout").permitAll();
-        http.authorizeRequests().antMatchers("/userInfo","/listarAgendas" , "/inicio").access("hasAnyRole('ROLE_ADMIN', 'ROLE_EJECUTIVO')");
-        http.authorizeRequests().antMatchers("/admin","/listarUsuarios", "/editarAgenda/*" ).access("hasRole('ROLE_ADMIN')" );
-        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
-        http.authorizeRequests().and().formLogin()
-                .loginProcessingUrl("/j_spring_security_check")
-                .loginPage("/login")
-                .defaultSuccessUrl("/inicio")
-                .failureUrl("/login?error=true")
-                .usernameParameter("soeid")
-                .passwordParameter("contrasena")
-                
-                
-                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/login");
-        
-        
-                
-        http.sessionManagement().invalidSessionUrl("/login");
+		http.authorizeRequests().antMatchers("/", "/welcome", "/login", "/logout").permitAll();
+		http.authorizeRequests().antMatchers("/userInfo", "/listarAgendas", "/inicio",
+				"/cargaMasiva", "/eliminacionMasiva", "/agregarAgenda")
+				.access("hasAnyRole('ROLE_ADMIN', 'ROLE_EJECUTIVO')");
+		http.authorizeRequests().antMatchers("/admin", "/listarUsuarios", "/agregarUsuario", 
+				"/editarUsuario/*" ,"/editarAgenda/*")
+				.access("hasRole('ROLE_ADMIN')");
+		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+		http.authorizeRequests().and().formLogin().loginProcessingUrl("/j_spring_security_check").loginPage("/login")
+				.defaultSuccessUrl("/inicio").failureUrl("/login?error=true").usernameParameter("soeid")
+				.passwordParameter("contrasena")
+
+				.and().logout().logoutUrl("/logout").logoutSuccessUrl("/login");
+
+		http.sessionManagement().invalidSessionUrl("/login");
 	}
 }
